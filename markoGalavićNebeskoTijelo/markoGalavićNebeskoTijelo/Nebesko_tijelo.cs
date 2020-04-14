d03f0c3f29f2321c050e57356546cb9b43d2dae3
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-04-14 16:15:34 +0200

    Add files via upload

---
+++ markoGalavićNebeskoTijelo/markoGalavićNebeskoTijelo/Nebesko_tijelo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavićNebeskoTijelo
+{
+    class Nebesko_tijelo
+    {
+         private double BrzinaOkoOsi;
+         private double BrzinaOkoSunca;
+
+        public override string ToString()
+        {
+            string ispis = "\nBrzina oko osi: " + BrzinaOkoOsi + "\nBrzina oko Sunca: " + BrzinaOkoSunca;
+            return ispis;
+        }
+        public Nebesko_tijelo()
+        {
+
+        }
+
+        public Nebesko_tijelo(double BrzinaOkoOsi, double BrzinaOkoSunca)
+        {
+            this.BrzinaOkoOsi = BrzinaOkoOsi;
+            this.BrzinaOkoSunca = BrzinaOkoSunca;
+        }
+
+
+
+
+        public double getBrzinaOkoOsi()
+        {
+            return BrzinaOkoOsi;
+        }
+        public void setBrzinaOkoOsi(double BrzinaOkoOsi)
+        {
+            this.BrzinaOkoOsi = BrzinaOkoOsi;
+        }
+        public double getBrzinaOkoSunca()
+        {
+            return BrzinaOkoSunca;
+        }
+        public void setBrzinaOkoSunca(double BrzinaOkoSunca)
+        {
+            this.BrzinaOkoSunca = BrzinaOkoSunca;
+        }
+
+
+    }
+}
---
+++ markoGalavićNebeskoTijelo/markoGalavićNebeskoTijelo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavićNebeskoTijelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BrzinaOkoOsi;
+            double BrzinaOkoSunca;
+
+            Nebesko_tijelo Planet = new Nebesko_tijelo(100, 300);
+            Nebesko_tijelo Satelit = new Nebesko_tijelo(500, 200);
+
+            Planet.setBrzinaOkoOsi(Planet.getBrzinaOkoOsi());
+            Planet.setBrzinaOkoSunca(Planet.getBrzinaOkoSunca());
+
+            Satelit.setBrzinaOkoOsi(Satelit.getBrzinaOkoOsi());
+            Satelit.setBrzinaOkoSunca(Satelit.getBrzinaOkoSunca());
+
+            
+            Console.WriteLine("Brzina Satelita oko osi je:" + BrzinaOkoOsi);
+            Console.WriteLine("Brzina Satelita oko Sunca je:" + BrzinaOkoSunca);
+            Console.ReadKey();
+        }
+    }
+    }
+
